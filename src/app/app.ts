697c2894408219260b039ddb5ae64e07901c35e2
Martin Mechtel <mechtel@iqb.hu-berlin.de>
2026-02-08 17:14:50 +0100

    Add languageTaggedText component with bidirectional binding

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,13 +1,26 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import {TranslatePipe} from '@ngx-translate/core';
+import {LanguageTaggedTextComponent} from './components/language-tagged-text/language-tagged-text.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, TranslatePipe],
+  imports: [RouterOutlet, TranslatePipe, LanguageTaggedTextComponent],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
 export class App {
   protected readonly title = signal('acp-editor');
+  data1 = signal<LanguageTaggedText> ([
+    {
+      lang: 'de',
+      value: 'yoyo jaja'
+    }
+  ]);
+  data2 = signal<LanguageTaggedText>([
+    {
+      lang: 'de',
+      value: 'bommerlunder'
+    }
+  ]);
 }
--- src/app/components/language-tagged-text/language-tagged-text.component.ts
+++ src/app/components/language-tagged-text/language-tagged-text.component.ts
@@ -1,12 +1,72 @@
-import { Component } from '@angular/core';
+import {Component, effect, input, linkedSignal, model, output, signal} from '@angular/core';
 import { TranslateModule } from '@ngx-translate/core';
+import {MatFormField, MatInput, MatLabel} from '@angular/material/input';
+import {form, FormField} from '@angular/forms/signals';
 
 @Component({
   selector: 'acp-language-tagged-text',
-  imports: [TranslateModule],
-  templateUrl: './language-tagged-text.html',
-  styleUrl: './language-tagged-text.scss',
+  imports: [TranslateModule, MatFormField, MatLabel, MatInput, FormField],
+  template: `
+    <form class="ltt-text-input">
+      @if (multiLine()) {
+        <mat-form-field class="ltt-full-width">
+          <mat-label>{{labelKey() | translate}}</mat-label>
+          <textarea matInput [placeholder]="placeholderKey() | translate"
+          [formField]="inputForm.text" ></textarea>
+        </mat-form-field>
+      } @else {
+        <mat-form-field class="ltt-full-width">
+          <mat-label>{{labelKey() | translate}}</mat-label>
+          <input matInput [placeholder]="placeholderKey() | translate" [formField]="inputForm.text">
+        </mat-form-field>
+      }
+    </form>
+  `,
+  styles: [
+    `
+      .ltt-text-input {
+        min-width: 150px;
+        max-width: 500px;
+        width: 100%;
+      }
+
+      .ltt-full-width {
+        width: 100%;
+      }
+    `
+    ],
 })
 export class LanguageTaggedTextComponent {
+  readonly textList = model.required<LanguageTaggedText>();
+  multiLine = input.required<boolean>();
+  placeholderKey = input<string>('');
+  labelKey = input<string>('');
+  textChanged = output<LanguageTaggedText>();
 
+
+  private readonly formModel = linkedSignal({
+    source: this.textList,
+    computation: (domainModel) => domainModel && domainModel[0] ?
+      {
+        "text": domainModel[0].value
+      } : {
+        "text": ""
+      }
+  });
+  inputForm = form(this.formModel)
+
+  constructor() {
+    effect(() => {
+      if (this.inputForm().valid()) {
+        this.textList.set(
+          [
+            {
+              lang: "de",
+              value: this.inputForm.text().value()
+            }
+          ]
+        )
+      }
+    });
+  }
 }
